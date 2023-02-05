a345db0aa1ae6bc8a7c7efdcffdebc62520e0a66
Garrett Gu <gu@utexas.edu>
2023-02-04 20:39:02 -0600

    add zz encoding, fltk

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod test;
 pub mod color;
+pub mod zz;
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("Hello, world!");
     jpeg::test::aaa();
     println!("{:?}", jpeg::color::RGB{r: 23, g: 55, b: 22}.ycbcr());
+    println!("{:?}", jpeg::zz::zz_to_idx(1));
+    println!("{:?}", jpeg::zz::idx_to_zz(0, 1));
 }
